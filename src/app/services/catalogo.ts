daab6347190ebae17c0e7029312d475794730f91
rodrigo09malheiro <rodrigo09malheiro@gmail.com>
2026-05-19 16:02:48 +0100

    Implementar Service para consumo dos ficheiros JSON do catálogo

--- src/app/services/catalogo.ts
+++ src/app/services/catalogo.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Catalogo {
-  
+
+  private basePath = 'assets/data';
+
+  constructor(private http: HttpClient) {}
+
+  getProdutos(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.basePath}/produtos.json`);
+  }
+
+  getCategorias(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.basePath}/categorias.json`);
+  }
+
+  getTipos(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.basePath}/tipos.json`);
+  }
+
+  getFaixasEtarias(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.basePath}/faixas-etarias.json`);
+  }
+
+  getProdutoById(id: number): Observable<any> {
+    return this.getProdutos().pipe(
+      map(produtos => produtos.find(p => p.id === id))
+    );
+  }
+
+  getProdutosPorCategoria(categoria: string): Observable<any[]> {
+    return this.getProdutos().pipe(
+      map(produtos => produtos.filter(p => p.categoria === categoria))
+    );
+  }
+
+  getProdutosDestaque(): Observable<any[]> {
+    return this.getProdutos().pipe(
+      map(produtos => produtos.filter(p => p.destaque === true))
+    );
+  }
 }
