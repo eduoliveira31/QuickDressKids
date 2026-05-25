db4f38cff7ea4a9aa57716c0371e2fc97eca96c2
Eduardo Oliveira <eduardooliveira2005@gmail.com>
2026-05-25 14:55:33 +0100

    Fix: Correção das rotas das Tabs e exibição do menu de navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,19 +7,7 @@ const routes: Routes = [
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
-    path: 'catalogo',
-    loadChildren: () => import('./catalogo/catalogo.module').then( m => m.CatalogoPageModule)
-  },
-  {
-    path: 'carrinho',
-    loadChildren: () => import('./carrinho/carrinho.module').then( m => m.CarrinhoPageModule)
-  },
-  {
-    path: 'perfil',
-    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
-  },
-  {
-    path: 'produto-detalhe/:id', /* <--- A ÚNICA MUDANÇA É AQUI (Adicionámos o /:id) */
+    path: 'produto-detalhe/:id',
     loadChildren: () => import('./produto-detalhe/produto-detalhe.module').then( m => m.ProdutoDetalhePageModule)
   }
 ];
